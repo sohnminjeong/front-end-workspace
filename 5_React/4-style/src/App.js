fadcb302328b54ce78289f582a066f1a5b887911
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-04-11 14:10:09 +0900

    4-style

---
+++ 5_React/4-style/src/App.js
@@ -0,0 +1,26 @@
+import "./asset/style.scss";
+import style from "./asset/style.module.css";
+import styled from "styled-components";
+
+// component 방식으로 만들기
+const Div = styled.div`
+  background-color: darkcyan;
+  color: white;
+  font-size: 6rem;
+`;
+
+const App = () => {
+  return (
+    <>
+      <div /*style={{ backgroundColor: "black", color: "white" }}*/
+        className="class"
+        /*id="id"*/
+      >
+        <h1 className={style.deco}>Hello, React Styling!</h1>
+      </div>
+      <Div>Styled Components!!</Div>
+    </>
+  );
+};
+
+export default App;
